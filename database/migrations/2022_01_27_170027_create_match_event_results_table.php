b28462bee7fbb359e715fe0dda689d7f340d2975
Dipen Modi <moddipen@gmail.com>
2022-01-27 17:01:24 +0530

    Betting Module

--- database/migrations/2022_01_27_170027_create_match_event_results_table.php
+++ database/migrations/2022_01_27_170027_create_match_event_results_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMatchtypeeventResultsTable extends Migration
+class CreateMatchEventResultsTable extends Migration
 {
     /**
      * Run the migrations.
